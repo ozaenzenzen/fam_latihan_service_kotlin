4c9420f17468e3cb3ea34014794505faf7068fb4
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-23 21:20:21 +0700

    update background service

--- app/src/main/java/com/example/myservice/MainActivity.kt
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
     }
 }
